3598067cf9da465e0285e29b566b972b4153f71d
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-09 16:04:31 +0100

    Inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,19 +9,24 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *newnode;
-newnode = malloc(sizeof(binary_tree_t));
-if (newnode == NULL)
-return (NULL);
-newnode->n = value;
-newnode->parent = parent;
-if (parent == NULL)
-return (NULL);
-if (parent->right != NULL)
-{
-newnode->right = parent->right;
-parent->right->parent = newnode;
-}
-parent->right = newnode;
-return (newnode);
+	binary_tree_t *new;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+	{
+	return (NULL);
+	}
+
+	if (parent->right != 0)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+
+	parent->right = new;
+	return (new);
 }
