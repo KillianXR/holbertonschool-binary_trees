7de8cc9b91192d865b8268ea046a1310906d260f
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-09 14:48:16 +0100

    fix the file

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,22 +16,13 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
-struct binary_tree_s
-{
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -50,4 +43,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif
+
