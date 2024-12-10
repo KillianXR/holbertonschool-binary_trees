a11783e1092dd42166701428e72d571df75006a7
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-10 13:16:23 +0100

    fix betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,14 +7,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
- size_t depth;
- depth = 0;
- if (tree == NULL)
-  return (0);
- while(tree->parent != NULL)
- {
-  depth++;
-  tree = tree->parent;
- }
- return (depth);
+size_t depth;
+depth = 0;
+if (tree == NULL)
+return (0);
+while (tree->parent != NULL)
+{
+depth++;
+tree = tree->parent;
+}
+return (depth);
 }
